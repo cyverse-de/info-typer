35896580229e32677215fa8aa8de89d0048d3942
Ian McEwen <mian@cyverse.org>
2017-03-16 12:59:10 -0700

    Upgrade common-cli

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
                    :exclusions [[org.slf4j/slf4j-log4j12]
                                 [log4j]]]
                  [org.cyverse/clojure-commons "2.8.0" :exclusions [commons-logging]]
-                 [org.cyverse/common-cli "2.8.0"]
+                 [org.cyverse/common-cli "2.8.1"]
                  [org.cyverse/heuristomancer "2.8.2"]
                  [org.cyverse/service-logging "2.8.0"]
                  [org.cyverse/event-messages "0.0.1"]]
